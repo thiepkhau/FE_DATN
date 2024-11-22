e0a3e8878eb0f6ad7239dd85e3ab5c8da556a46a
thiepkhau <ngocthiep213@gmail.com>
2024-11-22 15:58:09 +0700

    update components, service, api

--- app/apis/service/createServiceType.ts
+++
@@ -1,6 +0,0 @@
-// In createService.ts file
-import api from '@/utils/api';
-
-export const createServiceType = async (serviceData: { name: string }) => {
-	return await api.post('/service-type/add-service-type', serviceData);
-};
