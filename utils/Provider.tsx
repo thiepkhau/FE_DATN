27bfea5ce8a459d7dba51cb3e593ea595f07a8d7
thiepkhau <ngocthiep213@gmail.com>
2024-12-20 21:36:34 +0700

    tắt công cụ hỗ trojw

--- utils/Provider.tsx
+++ utils/Provider.tsx
@@ -1,14 +1,9 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { ReactNode, useState } from 'react';
 
 export default function Provider({ children }: { children: ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient());
-	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={true} />
-			{children}
-		</QueryClientProvider>
-	);
+	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
